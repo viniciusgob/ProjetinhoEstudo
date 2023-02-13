7e28a6ced703edf30416617b0b111f30f7e41b40
Vinicius Barros <awake251@gmail.com>
2023-02-13 18:40:57 -0300

    commit

---
+++ src/ProjetinhoEstudo/introducao/Aula11EstruturasDeRepeticao01.java
@@ -0,0 +1,20 @@
+package ProjetinhoEstudo.introducao;
+
+public class Aula11EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        //while, do while, for
+        int count = 12;
+        while (count < 10) {
+            System.out.println(++count);
+            //count = count +1;
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while" + ++count);
+        } while (count < 10);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For "+i);
+        }
+    }
+}
