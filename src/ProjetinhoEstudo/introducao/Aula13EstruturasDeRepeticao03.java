7cda48ac66eaeb6090b68168366be751d25ba79a
Vinicius Barros <awake251@gmail.com>
2023-02-21 13:46:21 -0300

    commit

--- src/ProjetinhoEstudo/introducao/Aula13EstruturasDeRepeticao03.java
+++ src/ProjetinhoEstudo/introducao/Aula13EstruturasDeRepeticao03.java
@@ -5,7 +5,7 @@ public class Aula13EstruturasDeRepeticao03 {
         //IMPRIMA OS PRIMEIROS 25 NUMEROS DE UM DADO VALOR. POR EXEMPLO 50
         int valorMax = 50;
         for (int i = 0; i <= valorMax; i++) {
-            if (i > 25){
+            if (i > 25) {
                 break;
             }
             System.out.println(i);
