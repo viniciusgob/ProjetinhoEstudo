ea79a0b8bb14773c9a32ad169cc129374e203dbd
Vinicius Barros <awake251@gmail.com>
2023-02-21 17:10:36 -0300

    Aula 14 commit

---
+++ src/ProjetinhoEstudo/introducao/Aula15EstruturasDeRepeticao05.java
@@ -0,0 +1,14 @@
+package ProjetinhoEstudo.introducao;
+
+public class Aula15EstruturasDeRepeticao05 {
+    public static void main(String[] args) {
+        double valorTot = 50000;
+        for (int parcela = (int)valorTot; parcela >= 1; parcela--) {
+            double valorParcela = valorTot / parcela;
+            if(valorParcela < 1000){
+                continue;
+            }
+            System.out.println("Parcela: "+parcela+" R$"+valorParcela);
+        }
+    }
+}
